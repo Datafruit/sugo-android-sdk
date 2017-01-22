4a9a6394c858b68bc49bd77f2889d63291c2e8ce
ouwenjie <going.ouwenjie@gmail.com>
2017-01-22 10:04:19 +0800

    fix #47
    fix #47

--- src/main/java/io/sugo/android/mpmetrics/SugoActivityLifecycleCallbacks.java
+++ src/main/java/io/sugo/android/mpmetrics/SugoActivityLifecycleCallbacks.java
@@ -11,6 +11,9 @@ import android.os.Looper;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.sugo.android.viewcrawler.GestureTracker;
 
 @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
@@ -23,13 +26,27 @@ import io.sugo.android.viewcrawler.GestureTracker;
     private final SugoAPI mMpInstance;
     private final SGConfig mConfig;
 
+    private final List<Activity> mSurviveActivities = new ArrayList<>();
+
     public SugoActivityLifecycleCallbacks(SugoAPI mpInstance, SGConfig config) {
         mMpInstance = mpInstance;
         mConfig = config;
     }
 
     @Override
-    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {}
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+        mSurviveActivities.add(activity);
+        if (mSurviveActivities.size() == 1) {
+            JSONObject props = new JSONObject();
+            try {
+                props.put(SGConfig.FIELD_PAGE, activity.getPackageName() + "." + activity.getLocalClassName());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            mMpInstance.track("launch_event", props);    // 启动了第一个 Activity，说明是应用被启动
+        }
+    }
+
     @Override
     public void onActivityStarted(Activity activity) {
         if (android.os.Build.VERSION.SDK_INT >= SGConfig.UI_FEATURES_MIN_API && mConfig.getAutoShowMixpanelUpdates()) {
@@ -42,7 +59,7 @@ import io.sugo.android.viewcrawler.GestureTracker;
 
     @Override
     public void onActivityResumed(Activity activity) {
-        mPaused = true;
+        mPaused = false;
         boolean wasBackground = !mIsForeground;
         mIsForeground = true;
 
@@ -52,6 +69,14 @@ import io.sugo.android.viewcrawler.GestureTracker;
 
         if (wasBackground) {
             // App is in foreground now
+            // App 从 background 状态回来，是被唤醒
+            JSONObject props = new JSONObject();
+            try {
+                props.put(SGConfig.FIELD_PAGE, activity.getPackageName() + "." + activity.getLocalClassName());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            mMpInstance.track("awake_event", props);
         }
 
         try {
@@ -63,8 +88,9 @@ import io.sugo.android.viewcrawler.GestureTracker;
             e.printStackTrace();
         }
     }
+
     @Override
-    public void onActivityPaused(Activity activity) {
+    public void onActivityPaused(final Activity activity) {
         mPaused = true;
         if (check != null) {
             mHandler.removeCallbacks(check);
@@ -74,6 +100,13 @@ import io.sugo.android.viewcrawler.GestureTracker;
             public void run() {
                 if (mIsForeground && mPaused) {
                     mIsForeground = false;
+                    JSONObject props = new JSONObject();
+                    try {
+                        props.put(SGConfig.FIELD_PAGE, activity.getPackageName() + "." + activity.getLocalClassName());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    mMpInstance.track("in_background_event", props);        // App 进入后台运行状态
                     mMpInstance.flush();
                 }
             }
@@ -87,15 +120,26 @@ import io.sugo.android.viewcrawler.GestureTracker;
             e.printStackTrace();
         }
     }
+
     @Override
     public void onActivityStopped(Activity activity) { }
+
     @Override
-    public void onActivityDestroyed(Activity activity) {}
+    public void onActivityDestroyed(Activity activity) {
+        mSurviveActivities.remove(activity);
+        if (mSurviveActivities.isEmpty()) {
+            JSONObject props = new JSONObject();
+            try {
+                props.put(SGConfig.FIELD_PAGE, activity.getPackageName() + "." + activity.getLocalClassName());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            mMpInstance.track("exit_event", props);     // 最后一个被摧毁的 Activity，是应用被退出
+        }
+    }
+
     @Override
     public void onActivitySaveInstanceState(Activity activity, Bundle outState) { }
-
-
-
 
 
 }
