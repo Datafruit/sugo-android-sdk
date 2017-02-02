14fc9e22843428783763d7d3d3165b435814877f
xianjingfeng <583872483@qq.com>
2017-02-02 14:38:17 +0800

    fix bug

--- src/main/java/io/sugo/android/mpmetrics/SGConfig.java
+++ src/main/java/io/sugo/android/mpmetrics/SGConfig.java
@@ -386,8 +386,8 @@ public class SGConfig {
     }
 
     public String getWebRoot() {
-        if (webRoot == null || webRoot.trim().equals(""))
-            webRoot = "''";
+        if (webRoot == null || webRoot.equals(""))
+            webRoot = " ";
         return webRoot;
     }
 
