59be1269705ad39d51c3c161b147957a0b727953
Ouwenjie <going.ouwenjie@gmail.com>
2017-05-09 15:09:36 +0800

    demo update

--- app/src/main/java/io/sugo/sdkdemo/activity/MainActivity.java
+++ app/src/main/java/io/sugo/sdkdemo/activity/MainActivity.java
@@ -23,7 +23,6 @@ import com.google.zxing.integration.android.IntentResult;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import io.sugo.android.mpmetrics.SGConfig;
 import io.sugo.android.mpmetrics.SugoAPI;
 import io.sugo.sdkdemo.R;
 
@@ -61,14 +60,14 @@ public class MainActivity extends AppCompatActivity {
 //        SugoAPI.setSuperPropertiesOnceBeforeStartSugo(this, "key-once", "value-once22");
 //        SugoAPI.setSuperPropertiesBeforeStartSugo(this, "key", "value");
 
-        SugoAPI.startSugo(this, SGConfig.getInstance(this)
-                .setToken("4e60562fd9814709d4b1468a48be62a2")
-                .setEventsEndPoint("http://dev220.sugo.net:80/post?locate=com_Sy2G_0R9g_project_Bk65jbIpg")
-                .logConfig());
-
 //        SugoAPI.startSugo(this, SGConfig.getInstance(this)
-//                .setToken("2dc8aa74c5727067cca466af7d966116")
-//                .setProjectId("com_HyoaKhQMl_project_rJIXzFOpe")
+//                .setToken("21ebb5c4f5123fbebe5c82edc0c51f1e")
+//                .setEventsEndPoint("http://dev220.sugo.net:80/post?locate=com_SJLnjowGe_project_H1suqID0l")
+//                .logConfig());
+//
+//        SugoAPI.startSugo(this, SGConfig.getInstance(this)
+//                .setToken("a18a1b870f7b5ee6e74fca827a2a1d4d")
+//                .setEventsEndPoint("http://collect.sugo.io/post?locate=com_SJLnjowGe_project_Hyv0LtEae")
 //                .logConfig());
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
