fad0b12b2a1bcd8f475a8407fff0ba3a44a31168
xianjingfeng <583872483@qq.com>
2018-06-06 11:57:47 +0800

    support react native

---
+++ library-reactnative/src/main/java/io/sugo/android/reactnative/RNSugo.java
@@ -0,0 +1,37 @@
+package io.sugo.android.reactnative;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by fengxj on 30/5/18.
+ */
+public class RNSugo implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new RNSugoModule(reactContext));
+
+        return modules;
+    }
+
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(
+            ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
