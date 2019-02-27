3c9f007f9d563d64893349d67963da77c9d4d2ab
chenyy <2543030051@qq.com>
2019-02-27 11:40:08 +0800

    fix SGConfig VERSION

--- library-wxj/src/main/java/io/sugo/android/mpmetrics/SGConfig.java
+++ library-wxj/src/main/java/io/sugo/android/mpmetrics/SGConfig.java
@@ -95,7 +95,7 @@ public class SGConfig {
     // Unfortunately, as long as we support building from source in Eclipse,
     // we can't rely on BuildConfig.SUGO_VERSION existing, so this must
     // be hard-coded both in our gradle files and here in code.
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "2.7.3";
 
     public static boolean DEBUG = false;
 
