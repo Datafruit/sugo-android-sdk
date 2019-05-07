bd42e631689f4c7c5f1ab4ec0f147a410f0cd41a
chenyy <2543030051@qq.com>
2019-05-07 20:03:18 +0800

    save tag

--- app/src/main/java/io/sugo/sdkdemo/App.java
+++ app/src/main/java/io/sugo/sdkdemo/App.java
@@ -9,7 +9,6 @@ import org.json.JSONObject;
 
 import io.sugo.android.mpmetrics.SGConfig;
 import io.sugo.android.mpmetrics.SugoAPI;
-import io.sugo.heatmap.SugoHeatMapManager;
 
 /**
  * @author Administrator
@@ -24,8 +23,6 @@ public class App extends Application {
         super.onCreate();
         SugoAPI.startSugo(this, SGConfig.getInstance(this).setSugoEnable(true).logConfig());
         SugoAPI instance = SugoAPI.getInstance(this);
-        SugoHeatMapManager manager = SugoHeatMapManager.getInstance(this);
-        manager.bindingHeatMapFunction();
         JSONObject jsonObject= new JSONObject();
         try {
             jsonObject.put("event_label", "过滤的数据(只包含类型不符合匹配规则的数据)上报到kafka的一个新的topic字段值匹配规则是，如果数字类型字段传了字符类型就设置为0数据字段值长度大于100个字符就认为是过大，截取前面的100个字符过滤的数据(只包含类型不符合匹配规则的数据)上报到kafka的一个新的topic");
